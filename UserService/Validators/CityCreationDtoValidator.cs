e0286052168537ec3272c1ac6e30926b2c6cfdcc
NatalijaGajic <nat.gaj98@gmail.com>
2021-06-16 12:07:02 +0200

    UserService: City and Role validators

---
+++ UserService/Validators/CityCreationDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Dtos.Cities;
+
+namespace UserService.Validators
+{
+    public class CityCreationDtoValidator: AbstractValidator<CityCreationDto>
+    {
+        public CityCreationDtoValidator()
+        {
+            RuleFor(x => x.CityName).NotNull().NotEmpty().MaximumLength(50).MinimumLength(3);
+
+        }
+    }
+}
---
+++ UserService/Validators/CityUpdateDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Dtos.Cities;
+
+namespace UserService.Validators
+{
+    public class CityUpdateDtoValidator: AbstractValidator<CityUpdateDto>
+    {
+        public CityUpdateDtoValidator()
+        {
+            RuleFor(x => x.CityName).NotNull().NotEmpty().MaximumLength(50).MinimumLength(3);
+
+        }
+    }
+}
