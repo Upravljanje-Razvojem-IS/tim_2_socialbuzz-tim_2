70b4578ac8b3d358d7e0cd0bf4169fe841cf9115
NatalijaGajic <nat.gaj98@gmail.com>
2021-06-17 02:49:21 +0200

    OcelotGateway: Path value condition in middleware

--- OcelotGateway/Middlewares/AccessTokenMiddleware.cs
+++ OcelotGateway/Middlewares/AccessTokenMiddleware.cs
@@ -2,6 +2,7 @@
 using OcelotGateway.Models;
 using OcelotGateway.Services;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,11 +17,22 @@ namespace OcelotGateway.Middlewares
             _authService = authService;
         }
 
+        //UpstreamPath /cities
+        private static ArrayList protectedPaths = new ArrayList {
+            "/cities"
+        };
+
+
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            AuthenticationResponse res = _authService.getAccessToken("").Result;
-            string token = res.Token;
-            context.Request.Headers["Authorization"] = "Bearer "+token;
+            string path = context.Request.Path.Value.ToString();
+            if (protectedPaths.Contains(path))
+            {
+                AuthenticationResponse res = _authService.getAccessToken("").Result;
+                string token = res.Token;
+                context.Request.Headers["Authorization"] = "Bearer " + token;
+            }
+
             //return;
             await next(context);
         }
